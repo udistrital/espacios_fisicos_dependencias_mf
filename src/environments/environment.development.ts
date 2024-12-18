0223de171fb69fec18f0ae154715f44b3ad83afb
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-12-18 15:30:57 -0500

    fix: Ajuste de variables de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production:false,
-    apiUrl: "http://localhost:4204/",
-    OIKOS_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8087/v2/',
-    OIKOS_MID: 'http://localhost:8080/v1/',
+    apiUrl: "https://pruebasoikosespaciosfisicos.portaloas.udistrital.edu.co/",
+    OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
+    OIKOS_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/espacios_fisicos_mid/v1/',
     DEPENDENCIAS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/dependencias_api/v1/'
 };
