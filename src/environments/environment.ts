ff6ab1fa097b8ae88370acebfe6749a7bb00bcd7
Diego Gutierrez <diagutierrezro@unal.edu.co>
2025-01-17 17:20:48 -0500

    fix: Se revierten cambios por prueba de despliegue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:false,
-    apiUrl: "http://localhost:4207/",
+    apiUrl: "http://localhost:4204/",
     OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
     OIKOS_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/espacios_fisicos_mid/v1/',
     DEPENDENCIAS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/dependencias_api/v1/'
